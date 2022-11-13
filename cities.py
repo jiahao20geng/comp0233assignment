e4a15b9b522ddefc7f955e923d9c91c9e56aa2cb
gengjiahao <1375209331@qq.com>
2022-11-13 19:50:17 +0000

    Define Class City

--- cities.py
+++ cities.py
@@ -1,15 +1,46 @@
 from typing import Dict, List, Tuple
+from dataclasses import dataclass
+from typing import Any
+from pathlib import Path
 
+@dataclass
 class City:
-    ...
+    def __init__(self,cityname:"str",country:"str",attendee:"int",lat:"float",lon:"float") ->"City":
+        if type(cityname)!=str:
+            raise TypeError("the city name must be a String!")
+        if type(country)!=str:
+            raise TypeError("the country must be a String!")
+        if type(attendee)!=int:
+            raise TypeError("the number of attendee must be integer!")
+        if type(lat)!=float:
+            raise TypeError("the latitude must be a decimal number!")
+        if type(lon)!=float:
+            raise TypeError("the longitude must be a decimal number!")
+        if attendee<=0:
+            raise ValueError("the number of attendee must be a positive integer!")
+        if lat<-90 or lat>90:
+            raise ValueError("latitude must be restricted to the -90 to 90 ranges")
+        if lon<-180 or lon>180:
+            raise ValueError("the longitude must be restricted to the -180 to 180 ranges")
+        
+        self.cityname=cityname
+        self.country=country
+        self.attendee=attendee
+        self.lat=lat
+        self.lon=lon
+        
+    
     def distance_to(self, other: 'City') -> float:
         raise NotImplementedError
-
+    
     def co2_to(self, other: 'City') -> float:
         raise NotImplementedError
 
+
+
+@dataclass
 class CityCollection:
-    ...
+    city: Any
 
     def countries(self) -> List[str]:
         raise NotImplementedError
@@ -37,4 +68,5 @@ class CityCollection:
 
     def plot_top_emitters(self, city: City, n: int, save: bool):
         raise NotImplementedError
+
 
